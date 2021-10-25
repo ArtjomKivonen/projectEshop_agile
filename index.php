3776cd1565889dfa06bb7f75ded3e4b875315e73
Artjom Kivonen <artjom.kivonen@ivkhk.ee>
2021-10-25 21:02:45 +0300

    new1

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ include_once 'inc/database.php';
 include_once 'model/Model.php';
 include_once 'model/ModelContact.php';
 include_once 'model/modelUser.php';
-// inclide_once 'model/modelCart.php';
+include_once 'model/modelCart.php';
 //...................
 
 include_once "controller/Controller.php";
 include_once 'controller/controllerUser.php';
-// include_once 'controller/controllerCart.php';
+include_once 'controller/controllerCart.php';
 //.......................
 include_once 'route/routing.php';
 
